a254a36be515b64fb979db84552f44cf74be3cbc
kcn00 <qasw2357@naver.com>
2022-04-17 00:46:38 +0900

    ver.3

--- The_Bedroom_at_Arles.cpp
+++ The_Bedroom_at_Arles.cpp
@@ -3,5 +3,5 @@
 void The_Bedroom_at_Arles() {
 	char title[30]="The Bedroom at Arles";
 	cout<<"Title: "<<title<<endl;
-	cout << "Link: https://www.vangoghmuseum.nl/en/collection/s0047V1962" << endl;
+	cout << "Site: https://www.vangoghmuseum.nl/en/collection/s0047V1962" << endl;
 }
--- The_Starry_Night.cpp
+++ The_Starry_Night.cpp
@@ -3,5 +3,5 @@
 void The_Starry_Night() {
 	char title[30]="The starry Night";
 	cout<<"Title: "<<title<<endl;
-	cout << "Link: https://www.vangoghmuseum.nl/en/art-and-stories/stories/where-is-the-starry-night" << endl;
+	cout << "Site: https://www.vangoghmuseum.nl/en/art-and-stories/stories/where-is-the-starry-night" << endl;
 }
