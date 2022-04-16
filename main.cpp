8cd7a172ca81229944063cd5fbb4468c6f0abbfd
kcn00 <qasw2357@naver.com>
2022-04-16 11:32:50 +0900

    ver.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include "header.h"
+void manu();
 
 int main() {
-	Almond_Blossom();
-	Sunflowers();
-	The_Starry_Night();
-	The_Bedroom_at_Arles();
+	manu();
 
 	return 0;
+}
+
+void manu() {
+	int mode;
+	cout << "print: 1, quit: other" << endl;
+	cin >> mode;
+	if (mode == 1) {
+		Almond_Blossom();
+		Sunflowers();
+		The_Starry_Night();
+		The_Bedroom_at_Arles();
+	}
+	else {
+		cout << "Bye" << endl;
+		quit(0);
+	}
 }
